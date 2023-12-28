2c3a3f759309d376c119d46d4748dbd390514356
Danil Markov <markov.danil.04@mail.ru>
2023-12-28 22:16:17 +0300

    1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4200);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  const port = process.env.PORT || 5001;
+
+  await app.listen(port, () => console.log(`server start on ${port}`));
 }
 bootstrap();
